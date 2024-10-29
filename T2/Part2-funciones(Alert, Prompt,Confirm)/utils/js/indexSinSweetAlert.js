b25a3df10773629e64e1861202786393544c038b
Mr-JonatanGomez <148049964+Mr-JonatanGomez@users.noreply.github.com>
2024-10-29 13:36:36 +0100

    Añadido algo de SweetAlert

--- T2/Part2-funciones(Alert, Prompt,Confirm)/utils/js/indexSinSweetAlert.js
+++ T2/Part2-funciones(Alert, Prompt,Confirm)/utils/js/indexSinSweetAlert.js
@@ -1,23 +1,18 @@
-//definimos nombre en vacio, para que no salte error por no estar definido antes
+//let nombre = "Jonatan"
+//let edad = 37
+//console.log(`Hola ${nombre} este es el primer programa JS`)
+let nombre = "" //definimos nombre en vacio, para que no salte error por no estar definido antes
 let edad = 0
 let estatura = 0.0
+// TRES MODOS DE INTERACTUAR CON EL USUARIO
 
-Swal.fire({
-  title: "Welcome to JonGo.DevIT!",
-  text: "You clicked the button!",
-  icon: "success"
-})
+//1. ALERT - mensaje que muestra lo que queramos y la opcion aceptar
+//2. PROMPT - permite introducir datos mediante el mensaje
 
+alert("Bienvenido a la pagina, mostrado con un simple alert")
+//su retorno es tipo void
 
-const { value: nombre } = await Swal.fire({
-  title: "Input your name",
-  input: "email",
-  inputLabel: "Your email address",
-  inputPlaceholder: "Enter your email address"
-})
-if (email) {
-  Swal.fire(`Entered email: ${email}`)
-}
+//su retorno es tipo STRING, aunque se puede cambiar al tipo que quieras de las siguientes formas
 
 //todo funcion flecha para verificar que no lleva numeros, (ESTAS FUNC, SIEMPRE ANTES DE USARLAS)
 const tieneNumero = (string) => {
