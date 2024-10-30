fd3268fe594b4e62d09c73f313672f7f41d44d83
Mr-JonatanGomez <148049964+Mr-JonatanGomez@users.noreply.github.com>
2024-10-30 10:46:16 +0100

    DWEC exercices

---
+++ T2/Part5-Ejercicios/utils/js/index.js
@@ -0,0 +1,14 @@
+//1. CREAR BARAJA CARTAS
+
+//2. INTENTAR MEZCLARLAS
+
+//3.SACARLAS DE UNA EN UNA HASTA NO TENER MAS
+
+//4. SACAR LAS CARTAS DEL PALO QUE PIDA EL USER
+
+//5.  SACAR UNA CARTA Y OBTENER SU VALOR
+let carta ="5C"
+console.log(carta.substring(0,carta.length-1));
+
+
+//6.
