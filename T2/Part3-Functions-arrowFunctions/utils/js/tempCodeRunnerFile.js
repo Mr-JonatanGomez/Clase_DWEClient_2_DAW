9d208188c95b71390a6bf297bddfb85a7b5287b3
Mr-JonatanGomez <148049964+Mr-JonatanGomez@users.noreply.github.com>
2024-10-31 11:01:28 +0100

    acabado ejercicios de DI

---
+++ T2/Part3-Functions-arrowFunctions/utils/js/tempCodeRunnerFile.js
@@ -0,0 +1,10 @@
+if (typeof ultimo == Number ) {
+  console.log("Lo siento, debe terminar en letra");
+  
+}else if(dni.length ==9){
+console.log("dni correcto");
+
+} else{
+  console.log();
+  
+}
