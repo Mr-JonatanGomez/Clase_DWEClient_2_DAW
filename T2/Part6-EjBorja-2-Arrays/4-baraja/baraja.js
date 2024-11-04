9d96f1e0088e354ba6c6edfd91bc02c72b56161d
Mr-JonatanGomez <148049964+Mr-JonatanGomez@users.noreply.github.com>
2024-11-04 18:49:26 +0100

    array terminado el 3

---
+++ T2/Part6-EjBorja-2-Arrays/4-baraja/baraja.js
@@ -0,0 +1,13 @@
+/*4. Crea un array vacío llamado baraja de tipo string. Mete de golpe 
+todas las cartas de la baraja francesa con el siguiente formato:
+
+```javascript
+1C,2C,3C,4C....,JC,QC,KC
+1D,2D,3D,4D....,JD,QD,KD
+1R,2R,3R,4R....,JR,QR,KR
+1P,2P,3P,4P....,JP,QP,KP
+```
+
+Una vez creado el array baraja las cartas para que se desordenen. 
+Puedes utilizar alguna librería externa como por ejemplo underscore
+*/
