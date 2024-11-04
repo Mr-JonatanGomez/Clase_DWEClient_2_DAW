25be80465fc0ad63bbc3f242220b45a71ae1c55a
Mr-JonatanGomez <148049964+Mr-JonatanGomez@users.noreply.github.com>
2024-11-04 17:13:47 +0100

    base terminado el 6

---
+++ T2/Part6-EjBorja-2-arraysEj/arrays.js
@@ -0,0 +1,22 @@
+/*
+
+1. Dado el array = [1,2,3,4,5,6,7,8,9,10]
+
+
+- Iterar por todos los elementos dentro de un array utilizando while y mostrarlos en pantalla.
+- Iterar por todos los elementos dentro de un array utilizando for y mostrarlos en pantalla.
+- Iterar por todos los elementos dentro de un array utilizando .forEach y mostrarlos en pantalla.
+- Mostrar todos los elementos dentro de un array sumándole uno a cada uno.
+- Calcular la media de todos los elementos del array
+
+ */
+
+
+
+let array = [1,2,3,4,5,6,7,8,9,10]
+
+while (array != null){
+
+    console.log(array);
+    
+}
