be84f84d9b94c57d19e906650959773c265b61d2
Mr-JonatanGomez <148049964+Mr-JonatanGomez@users.noreply.github.com>
2024-11-04 18:36:43 +0100

    array terminado el 3

---
+++ T2/Part6-EjBorja-2-Arrays/3-ordenar-array/ordenar.js
@@ -0,0 +1,10 @@
+/* 3. Crea un array con los siguientes valores: 
+
+```javascript
+const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+```
+
+Una vez tengas introducidos todos los valores realiza las siguientes tareas: 
+
+- Ordena el array y obten el valor máximo y mínimo
+- Obtén la medida de edad */
---
+++ T2/Part6-EjBorja-3-arrays-funciones/index.js
@@ -0,0 +1,31 @@
+/* 
+1. Dado el array = [1,2,3,4,5,6,7,8,9,10]
+
+
+- Iterar por todos los elementos dentro de un array utilizando while y mostrarlos en pantalla.
+- Iterar por todos los elementos dentro de un array utilizando for y mostrarlos en pantalla.
+- Iterar por todos los elementos dentro de un array utilizando .forEach y mostrarlos en pantalla.
+- Mostrar todos los elementos dentro de un array sumándole uno a cada uno.
+- Calcular la media de todos los elementos del array */
+
+let array1 = [1,2,3,4,5,6,7,8,9,10]
+let index = 0
+
+while (index < array1.length){
+  console.log(array1[index])
+  index++
+  
+}
+console.log(array1);
+
+
+/* let num1 = Number(prompt("Introduce un numero"))
+
+let num2 = Number(prompt("Introduce otro numero"))
+
+let result = num1 + num2
+
+alert(
+  `El resultado de sumar ${num1} y ${num2}, es ${result}\nGracias por usar este ejercicio`
+)
+6 */
