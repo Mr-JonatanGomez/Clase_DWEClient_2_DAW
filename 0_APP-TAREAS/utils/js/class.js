8515685cfecfa4159be8ada44ec8760865f283c4
Mr-JonatanGomez <148049964+Mr-JonatanGomez@users.noreply.github.com>
2024-12-01 18:06:40 +0100

    ejercicio Di

---
+++ 0_APP-TAREAS/utils/js/class.js
@@ -0,0 +1,17 @@
+class Tarea{
+    constructor(id,titulo, descripcion, fecha, prioridad){
+        this.id=id,
+        this.titulo=titulo,
+        this.descripcion=descripcion,
+        this.fecha=fecha,
+        this.prioridad= prioridad
+        /* this.completada=false
+        this.imagen=  imagen */
+    }
+}
+
+//todo creado mas o menos el registro
+
+
+
+//falta crear el js, para captar los datos y llevarlos a modo de cards al listado de tareas
--- 0_APP-TAREAS/utils/js/index.js
+++ 0_APP-TAREAS/utils/js/index.js
@@ -1,15 +1,52 @@
-class Tarea{
-    constructor(titulo, descripcion, fecha, prioridad){
-        this.id=id,
-        this.titulo=titulo,
-        this.descripcion=descripcion,
-        this.fecha=fecha,
-        this.prioridad= prioridad,
-        this.completada=completada,
-        this.imagen=  imagen
-    }
-}
+//todo Ya funciona guardar, ahora implementar solo guardar si hay datos
 
-//todo creado mas o menos el registro
+let tituloInput = document.querySelector("#tituloInput")
+let descripInput = document.querySelector("#descripInput")
+let maxDateInput = document.querySelector("#maxDateInput")
+
+let contadorIds = 1
+let btnGuardar = document.querySelector("#btnGuardar")
+
+btnGuardar.addEventListener("click", () => {
+  let id = contadorIds
+  let titulo = tituloInput.value
+  let descripcion = descripInput.value
+  let fechaMax = maxDateInput.value
+  let priority = document.querySelector("input[name='options']:checked")
+  prioridad = ""
+  if (priority) {
+    if (priority.value == 1) {
+      prioridad = "Urgente"
+    } else if (priority.value == 2) {
+      prioridad = "Media"
+    } else if (priority.value == 3) {
+      prioridad = "Baja"
+    }
+  } else {
+    Swal.fire({
+      position: "center",
+      icon: "error",
+      title: "No has seleccionado prioridad",
+      showConfirmButton: false,
+      timer: 2200
+    })
+  }
+
+  let nueva = new Tarea(contadorIds, titulo, descripcion, fechaMax, prioridad)
+  contadorIds++
+  console.log(
+    `id: ${nueva.id}\nTarea: ${nueva.titulo}\nDescrip: ${nueva.descripcion}\nFecha: ${nueva.fecha}\nPrioridad: ${nueva.prioridad}`
+  )
+  // clearInputs() implementar despues solo cuando usuario este guardado
+})
+
+function clearInputs() {
+  tituloInput.value = ""
+  descripInput.value = ""
+  maxDateInput.value = ""
+  prioridad = ""
+  let resetRadios = document.querySelectorAll("input[name='options']")
+  resetRadios.forEach((radio) => (radio.checked = false))
+}
 
 //falta crear el js, para captar los datos y llevarlos a modo de cards al listado de tareas
