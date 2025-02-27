905ac67db596afe90498223dced433d0b1fb9dc0
Mr-JonatanGomez <148049964+Mr-JonatanGomez@users.noreply.github.com>
2025-02-27 22:32:50 +0100

    Aprendiendo a jugar con los componentes y directivas Angular

--- T5_ANGULAR/tareas/src/app/app-routing.module.ts
+++ T5_ANGULAR/tareas/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListadoComponent } from './components/listado/listado.component';
+import { AsignaturasComponent } from './components/asignaturas/asignaturas.component';
+import { TecnologiasComponent } from './components/tecnologias/tecnologias.component';
+import { ErrorComponent } from './components/error/error.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  /* pagina inicio, siempre PRIMERO */
+  {path:"",redirectTo:"conocimientos", pathMatch:"full"},
+
+  {path:"conocimientos", component:ListadoComponent},
+  {path:"asignaturas", component:AsignaturasComponent},
+  {path:"tecnologias", component:TecnologiasComponent},
+
+  /* EN ultimo lugar la pagina de error, debe estar creada*/
+  {path:"**",component:ErrorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
