c7f45596b7b787ef4f4694980b3ae1e1212b6f3e
Mr-JonatanGomez <148049964+Mr-JonatanGomez@users.noreply.github.com>
2025-04-02 11:53:35 +0200

    filtro hecho

--- EVALUABLES/T3-API-service/src/app/components/filter/filter.component.ts
+++ EVALUABLES/T3-API-service/src/app/components/filter/filter.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Team } from '../../model/team';
 import { Router } from '@angular/router'
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-filter',
@@ -9,13 +10,22 @@ import { Router } from '@angular/router'
   styleUrl: './filter.component.css'
 })
 export class FilterComponent {
+  equiposFiltrados: Team[]=[]
 
-  seleccionCapacidad:number= 0
-  teams?: Team[]
+  seleccionCapacidad:string = ''
 
+  constructor(private dataService: DataService){}
+  
 
-
-  filtradoCapacidad(){
-   this.teams
-  }
+    filtradoCapacidad() {
+     //AQUI quiero el metodo para filtrar por capacidad de estadio intStadiumCapacity
+    this.equiposFiltrados = this.dataService.getTeamsByCapacity(this.seleccionCapacidad)
+    //ordenamos con sort
+    this.equiposFiltrados.sort((a, b) => {
+      return parseInt(a.intStadiumCapacity) - parseInt(b.intStadiumCapacity);
+    });
+    
+    }
+      
+    
 }
